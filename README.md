0b5a8fe8006c5330c3289f62d4854e4b2d43570e
tomnagengast <tnagengast@gmail.com>
2023-09-11 10:46:05 -0700

    Test out some response types

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Honogram
+
+Exploring Hono and Bun
+
+Routes
+
+- `/`: Basic route response
+- `/aye-pee-eyez`: Basic JSON response for an API
+- `/jammin`: Basic static asset/jamstack response
+
+## Resources
+
+- https://hono.dev/getting-started/bun
+- https://bun.sh/docs
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
+
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hono!"));
+app.use("/static/*", serveStatic({ root: "../" }));
 
-app.get("/hello", (c) => {
+app.get("/", (c) => {
+  return c.html(`
+  <h1>Hono</h1>
+  <ul>
+      <li><a href="/aye-pee-eyez">👀</a></li>
+      <li><a href="/jammin">🎸</a></li>
+  </ul>
+`);
+});
+
+app.get("/aye-pee-eyez", (c) => {
   return c.json({
-    message: `Hello!`,
+    message: `👀`,
   });
 });
+
+app.get("/jammin", serveStatic({ path: "./static/index.html" }));
 
 export default app;
